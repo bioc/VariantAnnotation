9b15bec58b105a5f63164355ab6f8febdabd0eb1
Valerie Obenchain <vobencha@fhcrc.org>
2012-12-10 21:16:24 +0000

    Fixed bug in predictCoding(). QUERYID was not mapping back to CollapsedVCF indices.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_11/madman/Rpacks/VariantAnnotation@71771 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-predictCoding.R
+++ R/methods-predictCoding.R
@@ -16,8 +16,12 @@ setMethod("predictCoding", c("VCF", "TranscriptDb", "ANY", "missing"),
     if (!is(alt, "DNAStringSetList"))
         stop("alt(<VCF>)[['ALT']] must be a DNAStringSetList")
     rd <- rep(rowData(query), elementLengths(alt))
-    callGeneric(rd, subject, seqSource, unlist(alt, use.names=FALSE), ...,
-                ignore.strand=ignore.strand) 
+    res <- callGeneric(rd, subject, seqSource, unlist(alt, use.names=FALSE),
+                ..., ignore.strand=ignore.strand)
+    ## adjust QUERYID for expansion of rowData
+    res$QUERYID <- rep(seq_len(length(alt)),
+                       elementLengths(alt))[res$QUERYID]
+    res
 })
 
 setMethod("predictCoding", c("GRanges", "TranscriptDb", "ANY", "DNAStringSet"),
